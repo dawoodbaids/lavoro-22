3eafebc837f0b61ca1ca2163852d7e16164291e7
MAjd <Zarqanmajd@gmail.com>
2023-12-30 18:14:39 +0300

    test 2

---
+++ lib/app/modules/auth/controllers/Type.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: unnecessary_overrides, file_names
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../core/utils/helpers/custom_snack_bar.dart';
+
+import '../../../routes/app_pages.dart';
+
+class SignupController extends GetxController {
+  final formKey = GlobalKey<FormState>();
+  bool isLoading = false;
+  bool employee = false;
+  bool isAccepted = false;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  Future<void> onSignupEmployee() async {
+    Get.toNamed(Routes.RegisterEmplyee);
+    employee = true;
+  }
+
+  Future<void> onSignupComapny() async {
+    Get.toNamed(Routes.RegisterCompany);
+    employee = false;
+  }
+
+
+  void goToSignInView() => Get.offNamed(Routes.SIGNIN);
+}
---
+++ lib/app/modules/auth/controllers/register_controller_company.dart
@@ -0,0 +1,195 @@
+// ignore_for_file: avoid_print
+
+//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:uuid/uuid.dart';
+
+import '../../../core/utils/helpers/custom_snack_bar.dart';
+import '../../../core/utils/helpers/system_helper.dart';
+import '../../../data/model/user_model.dart';
+import '../../../data/provider/firebase_image.dart';
+import '../../../data/provider/user_firebase.dart';
+import '../../../routes/app_pages.dart';
+
+class RegisterControllerCompany extends GetxController {
+  final formKey = GlobalKey<FormState>();
+  RxBool isLoading = false.obs;
+ //final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  late User? user;
+  late String selectedCountry2 = "Select Country";
+  late RxBool isExpanded = false.obs;
+  String? imagePath;
+  late String country;
+  late TextEditingController companyNameController;
+  late TextEditingController cemailController;
+  late TextEditingController jobDescriptionController;
+  late TextEditingController cphoneController;
+  late TextEditingController cpasswordController;
+  late TextEditingController cconfirmPasswordController;
+  late RxList<String> cselectedLanguages = <String>[].obs;
+
+  List<String> countries = [
+    "Select Country",
+    "irbid",
+    "aqaba",
+    "zarqa",
+    "amman",
+    "ma'an",
+    "balqa",
+    "maraq",
+    "tafilah",
+    "madaba",
+    "jerash",
+    "ajloun",
+    "karak"
+  ];
+
+  List<String> languageOptions = [
+    'Python',
+    'Java',
+    'JavaScript',
+    'C#',
+    'C++',
+    'Ruby',
+    'PHP',
+    'Swift',
+    'Kotlin',
+    'Dart',
+    'Go',
+    'Rust',
+    'TypeScript',
+    'SQL',
+    'HTML/CSS',
+    'Perl',
+    'Scala',
+    'Objective-C',
+    'Lua',
+    'Haskell',
+    'COBOL',
+    'Fortran',
+    'Prolog'
+  ];
+
+  @override
+  void onInit() {
+    cemailController = TextEditingController();
+    cpasswordController = TextEditingController();
+    cconfirmPasswordController = TextEditingController();
+    companyNameController = TextEditingController();
+    jobDescriptionController = TextEditingController();
+    cphoneController = TextEditingController();
+    country = countries[0];
+    cselectedLanguages = <String>[].obs;
+
+    super.onInit();
+  }
+
+  Future<void> signUp() async {
+    if (passwordConfirmed()) {
+      try {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: cemailController.text.trim(),
+          password: cpasswordController.text.trim(),
+        );
+        await onContinuePressed();
+
+        Get.offAllNamed(Routes.HOME);
+      } catch (e) {
+        print('Error during sign up: $e');
+        CustomSnackBar.error(message: e.toString());
+      }
+    } else {
+      print('Passwords do not match');
+      // Handle passwords not matching, e.g., show an error.
+    }
+  }
+
+  bool passwordConfirmed() {
+    return cpasswordController.text.trim() ==
+        cconfirmPasswordController.text.trim();
+  }
+
+  Future<void> onContinuePressed() async {
+    SystemHelper.closeKeyboard();
+    final isValidForm = formKey.currentState!.validate();
+    if (isValidForm == false) return;
+    if (imagePath == null || imagePath!.isEmpty) {
+      CustomSnackBar.warning(
+        title: "Profile picture",
+        message: "Please select a profile picture",
+      );
+      return;
+    }
+
+    isLoading(true);
+
+    try {
+      var uuid = const Uuid();
+      var v1 = uuid.v1();
+
+      final imageUrl = await FirebaseImage.uploadUserImage(
+        imagePath: imagePath!,
+        uid: v1,
+      );
+
+      UserAccount userAccount = UserAccount(
+        uid: v1,
+        username: companyNameController.text,
+        imageUrl: imageUrl!,
+        phoneNumber: cphoneController.text,
+        descrption: jobDescriptionController.text,
+        email: cemailController.text,
+        country: selectedCountry2,
+        selectedLanguages: cselectedLanguages,
+      );
+
+      await DatabaseFirestore.setUser(userAccount: userAccount);
+
+      UserAccount.currentUser = userAccount;
+
+      Get.offAllNamed(Routes.HOME);
+    } catch (e) {
+      CustomSnackBar.error(message: e.toString());
+    }
+
+    isLoading(false);
+  }
+
+  //Future<UserAccount?> getUser() async {
+    // try {
+     // final paths =
+     //     await _firestore.collection("users").doc(userInfo?.uid).get();
+     // Map<String, dynamic> data = paths.data() ?? {};
+//
+     // if (!completer.isCompleted) {
+     //   completer.complete(UserAccount.formJson(data));
+      }
+      // } on FirebaseException catch (e) {
+      //   if (!completer.isCompleted) {qضq
+      //     showDialog(
+      //       context: Get.overlayContext!,
+      //       builder: (BuildContext context) {
+      //         return AlertDialog(
+      //           title: Text('Firebase Exception'),
+      //           content: Text(e.message.toString()),
+      //         );
+      //       },
+      //     );
+      //   }
+  //  } catch (e) {
+     // if (!completer.isCompleted) {
+   //     print('salah ');
+        //Get.offAll(() => const NoInternetView());
+  ///  /  }
+    //}
+
+   // return completer.future;
+ /// }
+ // }
+ //  static User? get userInfo {
+   // return FirebaseAuth.instance.currentUser;
+// // }
+//}
